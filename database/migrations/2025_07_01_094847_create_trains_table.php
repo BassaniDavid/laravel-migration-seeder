a846677d1a0c052bff193c6e86286fa87f068e71
BassaniDavid <davbassan98@gmail.com>
2025-07-01 12:02:29 +0200

    feat: milestone 1 completed

---
+++ database/migrations/2025_07_01_094847_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("azienda", 50);
+            $table->string("stazione di partenza", 50);
+            $table->string("stazione di arrivo", 50);
+            $table->time("partenza");
+            $table->time("arrivo");
+            $table->string("codice treno", 20);
+            $table->string("numero carrozze", 4);
+            $table->boolean("in orario");
+            $table->boolean("cancellato");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
